af73a93aed8ffeb1b6a755d75b0648adf40be844
mochapot <rlawowls93@gmail.com>
2022-12-16 12:17:21 +0900

    라우터 추가적용 useSearchParams, useNavigate, useLocation

--- src/pages/Members.js
+++ src/pages/Members.js
@@ -1,6 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 const Members = (props) => {
+  // useNavigate Hook은 navigate()를 리턴한다.
+  // navigate(to, option) 이 있음.
+  // to     : 이동할 경로
+  // option : { replace : true/false, state: {} }
+  // repace 란 history 관리 ( false가 기본값 = 이전 history 교체 안함 history를 적립, true = 이전 history 교체)
+  // state 란 네비게이션 할 때 전달할 정보
+  // 이동한 후~~~ 에 location.state 로 이동 가능
+
+  const navigate = useNavigate();
+  const goHome = () => {
+    if (window.confirm("홈으로ㄱㄱ?")) {
+      navigate("/", { state: { from: "/member" } }); // (경로, 전달하고싶은내용) => 보통은 경로만 적고만다.
+    }
+  };
+
   // 이미지 사이즈 객체 설정
   const imgSize = { width: 90, height: 80 };
   const list = props.members.map((item, index) => {
@@ -27,6 +43,9 @@ const Members = (props) => {
       <div className="container">
         <div className="row">{list}</div>
       </div>
+      <button className="btn btn-primary" onClick={goHome}>
+        Go Home
+      </button>
     </div>
   );
 };
